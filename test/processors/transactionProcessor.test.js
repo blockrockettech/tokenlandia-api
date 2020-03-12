b365c47a1d01d9be3d44b6fd480538cc41f27770
Vincent de Almeida <vince0656@googlemail.com>
2020-03-12 10:00:28 +0000

    Failing tx test

--- test/processors/transactionProcessor.test.js
+++ test/processors/transactionProcessor.test.js
@@ -10,7 +10,7 @@ const TransactionProcessor = require('../../src/services/processors/transactionP
 
 const EscrowContractTruffleConf = require('../../src/truffleconf/escrow/TrustedNftEscrow');
 
-describe('Transaction Processor - ', async function () {
+describe.only('Transaction Processor - ', async function () {
 
   beforeEach(function () {
     this.now = Date.now();
@@ -108,5 +108,158 @@ describe('Transaction Processor - ', async function () {
       });
     });
   });
+
+  describe('Processing transactions for UPDATE_TOKEN job type', () => {
+    it('Correctly fires an update transaction', async () => {
+      const jobQueue = {
+        addStatusAndContextToJob: sinon.stub().onCall(0).resolves('success')
+      };
+
+      const mockUpdateIpfsHashResponse = {
+        hash: 'fff',
+        from: '0x0',
+        to: '0x456',
+        nonce: 1,
+        gasPrice: 5,
+        gasLimit: 10
+      };
+
+      const tokenlandiaService = {
+        updateIpfsHash: sinon.stub().onCall(0).resolves(mockUpdateIpfsHashResponse),
+      };
+
+      const chainId = 4;
+      const jobId = 'abc-123-def-456';
+      const tokenId = 1;
+      const job = {
+        chainId,
+        jobId,
+        tokenId,
+        jobType: JOB_TYPES.UPDATE_TOKEN,
+        context: {
+          [JOB_STATUS.ACCEPTED]: {
+            'token_id': 1,
+            'coo': 'USA',
+            'artist_initials': 'RSA',
+            'series': '002',
+            'design': '0003',
+            'name': 'token 1',
+            'description': 'token 1 description',
+            'image': '',
+            'artist': 'artist',
+            'artist_assistant': 'assistant',
+            'brand': 'brand',
+            'model': 'model',
+            'type': 'PHYSICAL_ASSET',
+            'product_id': 'USA-RSA-002-0003-113',
+            'product_code': 'USA-RSA-002-0003'
+          },
+          [JOB_STATUS.METADATA_CREATED]: {
+            metadataHash: 'QmPPi7piLxpzhcPUX6LSSEBRttS5VSn2AgMAiUtjn5Run9',
+            metadata: {
+              attributes: {
+                artist: 'artist',
+                artist_assistant: 'assistant',
+                artist_initials: 'RSA',
+                brand: 'brand',
+                coo: 'USA',
+                design: '0003',
+                model: 'model',
+                product_id: 'USA-RSA-002-0003-113',
+                series: '002',
+                token_id: 1
+              },
+              created: Math.floor(this.now / 1000),
+              description: 'token 1 description',
+              image: 'https://ipfs.infura.io/ipfs/QmXhGB4gbUnZgiaFSjL5r8EVHk63JdPasUSQPfZrsJ2cGf',
+              name: 'token 1',
+              type: 'PHYSICAL_ASSET'
+            }
+          }
+        }
+      };
+
+      const transactionProcessor = new TransactionProcessor(jobQueue, tokenlandiaService);
+      const result = await transactionProcessor.processJob(job);
+
+      result.should.be.deep.equal('success');
+
+      const metadataHash = job.context[JOB_STATUS.METADATA_CREATED].metadataHash;
+      sinon.assert.calledWith(tokenlandiaService.updateIpfsHash, tokenId, metadataHash);
+
+      sinon.assert.calledWith(jobQueue.addStatusAndContextToJob, chainId, jobId, JOB_STATUS.TRANSACTION_SENT, {
+        ...mockUpdateIpfsHashResponse,
+        gasPrice: mockUpdateIpfsHashResponse.gasPrice.toString(),
+        gasLimit: mockUpdateIpfsHashResponse.gasLimit.toString(),
+        metadataHash
+      });
+    });
+  });
+
+  describe('Invalid job type', () => {
+    it('Moves job into TX failed status', async () => {
+      const jobQueue = {
+        addStatusAndContextToJob: sinon.stub().onCall(0).resolves('success')
+      };
+
+      const chainId = 4;
+      const jobId = 'abc-123-def-456';
+      const tokenId = 1;
+      const job = {
+        chainId,
+        jobId,
+        tokenId,
+        jobType: null,
+        context: {
+          [JOB_STATUS.ACCEPTED]: {
+            'token_id': 1,
+            'coo': 'USA',
+            'artist_initials': 'RSA',
+            'series': '002',
+            'design': '0003',
+            'name': 'token 1',
+            'description': 'token 1 description',
+            'image': '',
+            'artist': 'artist',
+            'artist_assistant': 'assistant',
+            'brand': 'brand',
+            'model': 'model',
+            'type': 'PHYSICAL_ASSET',
+            'product_id': 'USA-RSA-002-0003-113',
+            'product_code': 'USA-RSA-002-0003'
+          },
+          [JOB_STATUS.METADATA_CREATED]: {
+            metadataHash: 'QmPPi7piLxpzhcPUX6LSSEBRttS5VSn2AgMAiUtjn5Run9',
+            metadata: {
+              attributes: {
+                artist: 'artist',
+                artist_assistant: 'assistant',
+                artist_initials: 'RSA',
+                brand: 'brand',
+                coo: 'USA',
+                design: '0003',
+                model: 'model',
+                product_id: 'USA-RSA-002-0003-113',
+                series: '002',
+                token_id: 1
+              },
+              created: Math.floor(this.now / 1000),
+              description: 'token 1 description',
+              image: 'https://ipfs.infura.io/ipfs/QmXhGB4gbUnZgiaFSjL5r8EVHk63JdPasUSQPfZrsJ2cGf',
+              name: 'token 1',
+              type: 'PHYSICAL_ASSET'
+            }
+          }
+        }
+      };
+
+      const transactionProcessor = new TransactionProcessor(jobQueue, null);
+      const result = await transactionProcessor.processJob(job);
+
+      result.should.be.deep.equal('success');
+
+      sinon.assert.calledWith(jobQueue.addStatusAndContextToJob, chainId, jobId, JOB_STATUS.TRANSACTION_FAILED, `Error: Unknown job type [null]`);
+    });
+  })
 
 });
