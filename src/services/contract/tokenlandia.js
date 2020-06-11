81d51032cc8d306d093adfa4e67c4c7d853bc4ad
Vincent de Almeida <vince0656@googlemail.com>
2020-06-11 14:35:31 +0100

    Adjusted the asset route to support the video latino token

--- src/services/contract/tokenlandia.js
+++ src/services/contract/tokenlandia.js
@@ -1,102 +1,18 @@
-const {ethers, constants, utils} = require('ethers');
+const TokenService = require('./token');
 const TokenLandiaTruffleConf = require('../../truffleconf/token/Tokenlandia');
-const {getContractAddressFromTruffleConf} = require('../../utils/truffle');
-const {getNetworkName} = require('@blockrocket/utils');
-const {getWallet, getHttpProvider} = require('../../web3/provider');
 
-function getBaseUrl(domain, networkName) {
-  if (networkName !== 'mainnet') {
-    return `https://${networkName}.${domain}`;
-  }
-  return `https://${domain}`;
-}
-
-class TokenLandia {
+class TokenLandia extends TokenService {
 
   constructor(chainId) {
-    this.chainId = chainId;
-
-    try {
-      this.signer = getWallet(chainId);
-      this.provider = getHttpProvider(chainId);
-    } catch (e) {
-      throw new Error(`Invalid chain ID [${chainId}]`);
-    }
-
-    this.contractAddress = getContractAddressFromTruffleConf(TokenLandiaTruffleConf, this.chainId);
-
-    if (!this.contractAddress) {
-      throw new Error(`No contract exists for chain ID '${chainId}'`);
-    }
-
-    this.contract = new ethers.Contract(
-      this.contractAddress,
-      TokenLandiaTruffleConf.abi,
-      this.signer
-    );
-  }
-
-  etherscanUrlForTokenId(tokenId) {
-    const baseUrl = getBaseUrl('etherscan.io', getNetworkName(this.chainId));
-    return `${baseUrl}/token/${this.contractAddress}?a=${tokenId}`;
-  }
-
-  etherscanUrlForTransaction(transactionHash) {
-    const baseUrl = getBaseUrl('etherscan.io', getNetworkName(this.chainId));
-    return `${baseUrl}/tx/${transactionHash}`;
-  }
-
-  openSeaUrlForTokenId(tokenId) {
-    const baseUrl = getBaseUrl('opensea.io', getNetworkName(this.chainId));
-    return `${baseUrl}/assets/${this.contractAddress}/${tokenId}`;
-  }
-
-  async attributesForTokenId(tokenId) {
-    return await this.contract.attributes(tokenId);
-  }
-
-  async tokenExists(tokenId) {
-    try {
-      // method call reverts for non exitance token ID
-      await this.contract.productCode(tokenId);
-      return true;
-    } catch (e) {
-      return false;
-    }
+    super(chainId, TokenLandiaTruffleConf);
   }
 
   async tokenIdForProductId(productId) {
-    return await this.contract.tokenIdForProductId(productId);
-  }
-
-  async tokenURI(tokenId) {
-    return await this.contract.tokenURI(tokenId);
-  }
-
-  async getBirthTransaction(tokenId) {
-    // From Zero for token ID
-    const filter = this.contract.filters.Transfer(constants.AddressZero, null, new utils.BigNumber(tokenId).toHexString());
-    filter.fromBlock = 0;
-    filter.toBlock = 'latest';
-
-    const events = await this.provider.getLogs(filter);
-    const {transactionHash, blockNumber} = events[0];
-    return {
-      transactionHash,
-      blockNumber,
-    };
-  }
-
-  async totalSupply() {
-    return await this.contract.totalSupply();
-  }
-
-  async mint(tokenId, recipient, productCode, ipfsHash) {
-    return this.contract.mintToken(tokenId, recipient, productCode, ipfsHash);
+    return this._tokenIdForProductId(productId, 'tokenIdForProductId');
   }
 
   async updateIpfsHash(tokenId, ipfsHash) {
-    return this.contract.updateIpfsHash(tokenId, ipfsHash);
+    return this._updateIpfsHash(tokenId, ipfsHash, 'updateIpfsHash');
   }
 }
 
--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 const TokenLandia = require('./contract/tokenlandia');
+const VideoLatino = require('./contract/videoLatino');
 const EscrowContract = require('./contract/escrow');
 const JobQueue = require('./job/jobQueue');
 const jobConstants = require('./job/jobConstants');
@@ -22,10 +23,12 @@ const jobQueue = new JobQueue(db);
 const ipfsService = new IpfsService(ipfsClient);
 
 const newTokenLandiaService = (chainId) => new TokenLandia(chainId);
+const newVideoLatinoService = (chainId) => new VideoLatino(chainId);
 const newEscrowService = (chainId) => new EscrowContract(chainId);
 
 module.exports = {
   newTokenLandiaService,
+  newVideoLatinoService,
   newEscrowService,
   jobQueue,
   jobValidator,
